199f0b1b8a4c70a61938b3f8e807919adb5ef428
Rokaya Nasr <rokanasr60@gmail.com>
2025-02-04 23:02:57 +0200

    not done

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
-
+const leftData = document.querySelector(".left-data");
 const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
+    navbar.classList.toggle("active");
+    mobileNav.classList.toggle("hamburger-active");
+};
+
+window.onload = () => {
+    let data;
+    fetch("data.json")
+        .then((res) => res.json())
+        .then((res) => {
+            data = [...res];
+            console.log(data);
+            drawData(data);
+            return data;
+        })
+        .catch((err) => console.log(err));
+
+    const drawData = (books) => {
+        console.log(books);
+        books.forEach((book) => {
+            let leftDiv = document.createElement("div");
+
+            if (book.id <= 4) {
+                leftDiv.classList.add("left-div", "mb-24");
+                leftDiv.innerHTML = `
+                <div class='book-data'>
+                  <h2>${book.title}</h2>
+                  <p>${book.description}</p>
+                  <span>${book.price}</span>
+                </div>
+                <img src=${book.image} alt=${"img" + book.id} />
+            `;
+            }
+            leftData.appendChild(leftDiv);
+        });
+    };
+    // leftDiv.innerHTML = `<img src=${} alt="img1" />
 };
 mobileNav.addEventListener("click", () => toggleNav());
