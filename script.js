d4eff3c529d1e229f41386db45483964e8eaaa66
Rokaya Nasr <rokanasr60@gmail.com>
2025-02-05 14:09:22 +0200

    right div done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mobileNav = document.querySelector(".hamburger");
 const navbar = document.querySelector(".menubar");
 const leftData = document.querySelector(".left-data");
+const rightData = document.querySelector(".right-data");
+
 const toggleNav = () => {
     navbar.classList.toggle("active");
     mobileNav.classList.toggle("hamburger-active");
@@ -22,6 +24,7 @@ window.onload = () => {
         console.log(books);
         books.forEach((book) => {
             let leftDiv = document.createElement("div");
+            let rightDiv = document.createElement("div");
 
             if (book.id <= 4) {
                 leftDiv.classList.add("left-div", "mb-24");
@@ -34,7 +37,21 @@ window.onload = () => {
                 <img src=${book.image} alt=${"img" + book.id} />
             `;
             }
+
+            if (book.id > 4) {
+                rightDiv.classList.add("right-div", "mb-24");
+                rightDiv.innerHTML = `
+                <img src=${book.image} alt=${"img" + book.id} />
+                <div class='book-data'>
+                  <h2>${book.title}</h2>
+                  <p>${book.description}</p>
+                  <span>${book.price}</span>
+                </div>
+            `;
+            }
+
             leftData.appendChild(leftDiv);
+            rightData.appendChild(rightDiv);
         });
     };
     // leftDiv.innerHTML = `<img src=${} alt="img1" />
